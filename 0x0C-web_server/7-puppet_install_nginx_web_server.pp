62c61f2d3ba3e8fd3fa6993826a62b5d9a1438ac
boak123 <boluwatifeakorede17@gmail.com>
2023-11-28 13:48:21 -0800

    updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -18,3 +18,8 @@ exec {'Hello':
 exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
